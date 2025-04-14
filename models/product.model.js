bb741dd59d2a4ce7ed7042d16e41ee7f427e1d97
rohulrez <rezayeerohullah@gmail.com>
2025-04-13 18:42:28 -0700

    Route for editing a product & updating it (adminstrator) logics added

--- models/product.model.js
+++ models/product.model.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const db = require('../data/database');
 
 class Product {
@@ -7,8 +8,7 @@ class Product {
         this.price =  +productData.price;
         this.description = productData.description;
         this.image = productData.image;
-        this.imagePath = `product-data/images/${productData.image}`;
-        this.imageUrl = `/products/assets/images/${productData.image}`;
+        this.updateImageData();
         if(productData._id) {
             this.id = productData._id.toString();
         };
@@ -22,6 +22,33 @@ static async findAll () {
     });
 }
 
+updateImageData () {
+    this.imagePath = `product-data/images/${this.image}`;
+    this.imageUrl = `/products/assets/images/${this.image}`;
+}
+
+static async findById(productId) {
+    let proId;
+    try {
+        proId = new mongodb.ObjectId(productId);
+    } catch(error) {
+        throw error;
+    }
+
+
+    const product = await db
+    .getDb()
+    .collection('products')
+    .findOne({_id: proId});
+    
+    if(!product) {
+        const error = new Error('Could not find product with provided id.');
+        error.code = 404;
+        throw error;
+    }
+    return new Product(product);
+}
+
 
 async save() {
     const productData = {
@@ -32,7 +59,25 @@ async save() {
         image: this.image
     }
 
-    await db.getDb().collection('products').insertOne(productData)
+
+    if(this.id) {
+        const productId = new mongodb.ObjectId(this.id)
+
+        if(!this.image) {
+        delete productData.image;
+        }
+
+        await db.getDb().collection('products').updateOne({_id: productId}, {
+            $set: productData
+        });
+    } else{
+        await db.getDb().collection('products').insertOne(productData)
+    }
+}
+
+async replaceImage (newImage) {
+    this.image = newImage;
+    this.updateImageData();
 }
 
 }
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -9,7 +9,13 @@ router.get('/products' ,adminController.getProducts);
 
 router.get('/products/new',adminController.getNewProduct);
 
-router.post('/products',imageUplaodMiddleware ,adminController.createNewProduct)
+router.post('/products',imageUplaodMiddleware ,adminController.createNewProduct);
+
+router.get('/products/:id',adminController.getUpdateProduct);
+
+router.post('/products/:id', imageUplaodMiddleware , adminController.updateProduct);
+
+// router.post('/products/:id', adminController.deleteProduct);
 
 
 
