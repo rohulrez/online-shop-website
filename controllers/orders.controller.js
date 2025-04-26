4548879b2536c075efda4cf8e3ce2a04dc5a531d
rohulrez <rezayeerohullah@gmail.com>
2025-04-26 16:29:06 -0700

    Stripe payment added && API key hundled

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
 const Order = require ('../models/order.model');
 const User = require ('../models/user.model');
 const { get } = require('../routes/auth-routes');
@@ -38,12 +41,39 @@ async function addOrder (req, res, next){
 
     req.session.cart = null;
     
-    res.redirect('/orders');
-
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: cart.items.map(function(item){
+           return { price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.product.title,
+              },
+              unit_amount: Math.round(item.product.price * 100),
+            },
+            quantity: item.quantity,
+          }}),
+        mode: 'payment',
+        success_url: 'http://localhost:3000/orders/success',
+        cancel_url: 'http://localhost:3000/orders/failure',
+      });
+    
+      res.redirect(303, session.url);
 }
+
+function getSuccess (req, res) {
+    res.render('customer/orders/success');
+}
+
+function getFailure (req, res) {
+    res.render('customer/orders/failure');
+}
+
 
 module.exports = {
     getOrders,
     addOrder,
+    getSuccess,
+    getFailure
 
 }
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -9,6 +9,9 @@ router.get('/', orderController.getOrders);
 
 router.post('/', orderController.addOrder);
 
+router.get('/success', orderController.getSuccess);
+
+router.get('/failure', orderController.getFailure);
 
 
 module.exports = router;
